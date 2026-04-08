314e0222b941ee0c8d336d58b4b630154eab7789
Saumya Shah <saumyashah609@gmail.com>
2026-04-08 19:53:04 +0530

    Refactor max_area and find_max to utilize caching for improved performance

--- 2-medium/0011_container_with_most_water/src/main.rs
+++ 2-medium/0011_container_with_most_water/src/main.rs
@@ -15,12 +15,16 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        let CACHE: HashMap<((usize, &i32), (usize, &i32)), usize> = HashMap::new();
+        let mut cache: HashMap<((usize, &i32), (usize, &i32)), usize> = HashMap::new();
         let mut prod: usize = 0;
 
         height.iter().enumerate().reduce(|a, b| {
-            let prod_a = Solution::find_max(a, height.clone());
-            let prod_b = Solution::find_max(b, height.clone());
+            let calc_a = Solution::find_max(a, height.clone(), cache.clone());
+            let prod_a = calc_a.0;
+            cache = calc_a.1;
+            let calc_b = Solution::find_max(b, height.clone(), cache.clone());
+            let prod_b = calc_b.0;
+            cache = calc_b.1;
             if prod_a > prod_b {
                 prod = prod_a;
                 a
@@ -33,24 +37,31 @@ impl Solution {
         prod as i32
     }
 
-    fn find_max(a: (usize, &i32), height: Vec<i32>) -> usize {
+    fn find_max<'b>(
+        a: (usize, &'b i32),
+        height: Vec<i32>,
+        mut hm_cache: HashMap<((usize, &'b i32), (usize, &'b i32)), usize>,
+    ) -> (usize, HashMap<((usize, &'b i32), (usize, &'b i32)), usize>) {
         let mut prod = 0;
-        height
-            .iter()
-            .enumerate()
-            .for_each(|s| {
-                let calc = Solution::calculate(a, s);
-                if prod <= calc {
-                    prod = calc
-                }
-            });
-        prod
+        height.iter().enumerate().for_each(|s| { // TODO: lifetime error (`height` does not live long enough borrowed value does not live long enough)
+            let calculated = Solution::calculate(a, s, hm_cache.clone());
+            let calc = calculated.0;
+            hm_cache = calculated.1;
+            if prod <= calc {
+                prod = calc
+            }
+        });
+        (prod, hm_cache)
     }
 
-    fn calculate(
-        from: (usize, &i32),
-        to: (usize, &i32),
-    ) -> usize {        
+    fn calculate<'a>(
+        from: (usize, &'a i32),
+        to: (usize, &'a i32),
+        mut hm_cache: HashMap<((usize, &'a i32), (usize, &'a i32)), usize>,
+    ) -> (usize, HashMap<((usize, &'a i32), (usize, &'a i32)), usize>) {
+        if hm_cache.contains_key(&(from, to)) {
+            return (*hm_cache.get(&(from, to)).unwrap(), hm_cache);
+        }
         let length = if from.0 > to.0 {
             (from.0 + 1) - (to.0 + 1)
         } else {
@@ -58,6 +69,7 @@ impl Solution {
         };
         let height = if from.1 < to.1 { from.1 } else { to.1 };
         let prod = length * (*height as usize);
-        prod
+        hm_cache.insert((from, to), prod);
+        (prod, hm_cache)
     }
 }
